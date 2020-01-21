0fe218f2653e29ee43395ad5dbd2ab3ab9ef31a7
bdjika <nsanjka@gmail.com>
2020-01-21 17:46:28 +0300

    added: settings for jwt auth

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from pentest_brain.views import PentesterViewSet, SphereViewSet, TagViewSet, CommandViewSet, CommandDocumentView
 
+
+import config.jwt_refresh
 
 router = routers.DefaultRouter()
 
@@ -18,6 +21,8 @@ router.register(r'search', CommandDocumentView, basename='Search')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    url(r'^api/auth/token/login/$', obtain_jwt_token),
+    url(r'^api/auth/token/refresh/$', refresh_jwt_token),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
